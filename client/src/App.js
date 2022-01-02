13e481f9499085e145278c06340f72e00d9cff20
ArcaneWright <77860571+arcanewright@users.noreply.github.com>
2022-01-01 23:56:58 -0500

    First steps - attempt to implement client and server communication

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import react, {useEffect, useState} from "react";
+import io from "socket.io-client"
 import './App.css';
 
-function App() {
+const socket = io("http://localhost:3001");
+
+const App = () => {
+
+  const getSocketID = () => {
+    return socket.id;
+  }
+
+
   return (
     <div className="App">
       <h1>Pop Game Site</h1>
+      <h2>ID: {}</h2>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -6,7 +6,12 @@ const PORT = process.env.PORT || 3001
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {cors: {
+    origin:"http://localhost:3000",
+    methods:["GET", "POST"],
+
+
+}});
 
 app.get("/", (req, res) =>{
     res.send("<h1>Pop Game Server</h1>")
