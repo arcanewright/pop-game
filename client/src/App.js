8ee04f0221c3dc930aae7ed45fea951f3ee9d6b3
ArcaneWright <77860571+arcanewright@users.noreply.github.com>
2022-01-02 12:18:12 -0500

    first steps into creating game rooms

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import react, {useEffect, useState} from "react";
-import io from "socket.io-client"
+import io from "socket.io-client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 const socket = io("http://localhost:3001");
 
+
 const App = () => {
   const [userID, setUserID] = useState(window.localStorage.getItem("userID"));
+  const [color, setColor] = useState("black")
+  const [currentRoom, setCurrentRoom] = useState("none");
+  const [players, setCurrentPlayers] = useState([]);
+
+  
+  socket.on(("newColor"), (newColor) => {
+    setColor(newColor);
+  })
+
+  socket.on(("roomUpdate"), (room) => {
+    setCurrentRoom(room.gameRoom);
+    setCurrentPlayers(room.players);
+  })
 
   useEffect(() => {
     if (!userID) {
@@ -22,13 +37,54 @@ const App = () => {
 
 
   return (
-    <div className="App">
-      <h1>Pop Game Site</h1>
-      <h2>My id is: {userID}</h2>
-      <button onClick={() => socket.emit("hello")}>Say hello</button>
-      <button onClick={() => setUserID(null)}>Change ID</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<StartScreen></StartScreen>}></Route>
+          <Route path=":room" element={<Room></Room>}>
+
+          </Route>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
+}
+
+const Room = ({}) => {
+  return (<div>
+    <h1>Room</h1>
+  </div>)
+}
+
+const StartScreen = ({roomCode, setRoomCode}) => {
+
+  const [inputRoomCode, setInputRoomCode] = useState("");
+  const [invalidCode, setInvalidCode] = useState(false)
+
+  const codeValidation = RegExp("")
+
+  const validateCode = (code) => {
+    if (code) {
+      
+    }
+  }
+
+  return (<div className="start-screen">
+    <h1>Pop Game Site</h1>
+    <div>
+      <h2>Room</h2>
+      <input type="text" value={roomCode} style={{color: invalidCode ? "red" : "black"}}onChange={(e) => {validateCode(e.target.value); setInputRoomCode(e)}}></input>
+    </div>
+    <div>
+      <button>Join Game</button>
+      <h3>Join an existing game as a player</h3>
+    </div>
+    <div>
+      <button>Create New Game</button>
+      <h3>Create a new game as the display</h3>
+    </div>
+  </div>)
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -13,7 +13,18 @@ const io = new Server(server, {cors: {
 
 }});
 
+let color = "yellow"
+
 let users = [];
+
+let games = [];
+
+const gamePrototype = {
+    gameRoom:"AAAA",
+    mainUser:"",
+    players:[],
+    state:""
+}
 
 app.get("/", (req, res) =>{
     res.send("<h1>Pop Game Server</h1>")
@@ -21,9 +32,33 @@ app.get("/", (req, res) =>{
 
 io.on("connection", (socket) => {
     console.log(`User ${socket.id} connected`)
+    socket.emit("newColor", color);
     users.push({socket: socket.id, user: "", userName: ""})
     socket.on("hello", () => {
         console.log(socket.id + " said hello!")
+    })
+    socket.on("askForColorChange", () => {
+        color = "rgb(" + Math.random()*256 + ", " + Math.random()*256 + ", " + Math.random()*255 + ")";
+        socket.emit("newColor", color);
+    })
+    socket.on("createGame", () => {
+        const newRoom = {gameRoom:"ABCD", mainUser:socket.id, players:[], state:""};
+        games.push(newRoom)
+        console.log("new game created: " + newRoom.gameRoom)
+        socket.emit("roomUpdate", newRoom)
+    })
+    socket.on("joinGame", (roomCode) => {
+        let room = games.find((e)=> e.gameRoom === roomCode);
+        if (room) {
+            room.players.push(socket.id);
+            console.log("Current players: " + room.players)
+            games[games.findIndex((e) => e.gameRoom === roomCode)] = room;
+            for (let playerID of room.players) {
+                io.in(playerID).emit("roomUpdate", room);
+            }
+            
+        }
+        
     })
     socket.on("changeID", (newID) => {
         const newUser = {...users.find((e, i) => e.socket === socket.id), user:newID}
